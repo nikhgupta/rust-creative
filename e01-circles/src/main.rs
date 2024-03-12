dee0335c2174bd7644931af7615f796e509c880e
Nikhil Gupta <me@nikhgupta.com>
2024-03-12 23:40:15 +0530

    use model methods to draw particles; add more keyboard controls

--- e01-circles/src/main.rs
+++ e01-circles/src/main.rs
@@ -4,6 +4,7 @@ use nannou::rand::{Rng, SeedableRng};
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
+const MARGIN: f32 = 40.0;
 
 fn main() {
     nannou::app(model)
@@ -12,32 +13,38 @@ fn main() {
         .run();
 }
 
-struct Dot {
+struct Particle {
     x: f32,
     y: f32,
     radius: f32,
     color: Rgba,
 }
 
-struct Model {
-    seed: u64,
-    particles: Vec<Dot>,
+impl Particle {
+    // display method to draw each particle
+    fn display(&self, draw: &nannou::Draw) {
+        draw.ellipse()
+            .x_y(self.x, self.y)
+            .radius(self.radius)
+            .color(self.color);
+    }
 }
 
-// convert the earlier code to use an impl block. (do compare the diff side by side.)
-// https://doc.rust-lang.org/rust-by-example/trait/impl.html
-// this allows us to group methods together
-// and use the `self` keyword to refer to the struct
+struct Model {
+    seed: u64,
+    num: u32,
+    particles: Vec<Particle>,
+}
+
 impl Model {
     fn new() -> Self {
-        let seed = 0;
         let particles = Vec::new();
 
-        // the following is a shorthand for
-        // Model { seed: seed, particles: particles }
-        // you can initialize the values here as well, e.g.
-        // Model { seed: 0, particles: Vec::new() }
-        Model { seed, particles }
+        Model {
+            seed: 0,
+            num: 100,
+            particles,
+        }
     }
 
     // fn rng(&self) -> StdRng {
@@ -46,42 +53,51 @@ impl Model {
 
     fn reset_seed(&mut self) -> &mut Self {
         self.seed = random_range(0, 1000000);
-        // Rust returns the last value in the block
-        // (be careful that it does not end with a semicolon)
-        // this allows us to chain methods together and
-        // hook into the builder pattern
         self
     }
 
-    fn generate(&mut self) -> &mut Self {
+    fn generate(&mut self, app: &App) -> &mut Self {
         self.particles = Vec::new();
         let mut rng = StdRng::seed_from_u64(self.seed);
 
-        // for now we will add a single dot in this refactor
-        // we will add a loop later to introduce more dots
-        let dot = Dot {
-            x: rng.gen_range(-0.4..0.4) * WIDTH as f32 / 2.0,
-            y: rng.gen_range(-0.4..0.4) * HEIGHT as f32 / 2.0,
-            radius: rng.gen_range(40.0..200.0),
-            color: Rgba::new(
-                rng.gen(),
-                rng.gen(),
-                rng.gen(),
-                // f32 required here.
-                // vs-code allows us to see the function signature,
-                // which is helpful in such cases and helped in understanding
-                // how to define the type for this function.
-                0.25 * 0.75 + rng.gen::<f32>(),
-            ),
-        };
+        // instead of relying on fixed width and height,
+        // we want to use window dimensions on the fly.
+        // added a margin to the window dimensions to avoid particles
+        // from sticking to the edges of the window.
+        let rect = app.window_rect().pad(MARGIN);
 
-        // we can use the push method to add a single dot to the vector
-        // remember to set self.particles to Vec::new() before adding the dot
-        // otherwise, the dots will accumulate on pressing Space
-        self.particles.push(dot);
+        // we want to add a bunch of particles to the model
+        for _ in 0..self.num {
+            let dot = Particle {
+                x: rng.gen_range(-0.5..0.5) * rect.w(),
+                y: rng.gen_range(-0.5..0.5) * rect.h(),
+                radius: rng.gen_range(40.0..200.0),
+                color: Rgba::new(
+                    rng.gen(),
+                    rng.gen(),
+                    rng.gen(),
+                    0.25 * 0.75 + rng.gen::<f32>(),
+                ),
+            };
+            self.particles.push(dot);
+        }
 
-        // return self to allow chaining methods
         self
+    }
+
+    // instead, of using a for loop to draw each particle in view,
+    // we can write a display function in the impl block of Model
+    // and delegate the drawing to each particle.
+    // this way, we can keep the view function clean and simple.
+    fn display(&self, draw: &nannou::Draw) {
+        for particle in &self.particles {
+            // delegate the drawing to each particle
+            particle.display(&draw);
+        }
+    }
+
+    fn reset(&mut self, app: &App) {
+        self.reset_seed().generate(&app);
     }
 }
 
@@ -98,19 +114,28 @@ fn model(app: &App) -> Model {
     // which annoys rust-analyzer
     // for now, I am using this, but will try to use the builder pattern later
     let mut model = Model::new();
-    model.reset_seed().generate();
+    model.reset_seed().generate(app);
     model
 }
 
-fn key_pressed(_app: &App, model: &mut Model, key: Key) {
+fn key_pressed(app: &App, model: &mut Model, key: Key) {
     match key {
         Key::Space => {
-            // look how simple it became to reset the state!
-            // if this starts getting complex, we can add a method to the Model
-            // for resetting the entire state and move this code over there.
-            model.reset_seed().generate();
+            model.reset(app);
         }
-        // unmatched keys are ignored
+        // add keyboard controls to increase or decrease the number of particles
+        // for now, this resets all particles.
+        // later, we will add or remove particles without resetting all of them.
+        Key::Up => {
+            model.num += 10;
+            model.reset(app);
+        }
+        Key::Down => {
+            if model.num > 10 {
+                model.num -= 10;
+                model.reset(app);
+            }
+        }
         _ => (),
     }
 }
@@ -121,13 +146,8 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(SNOW);
 
-    // looping through the particles and drawing them based on their properties
-    for particle in &model.particles {
-        draw.ellipse()
-            .x_y(particle.x, particle.y)
-            .radius(particle.radius)
-            .color(particle.color);
-    }
+    // use display method to draw all particles
+    model.display(&draw);
 
     draw.to_frame(app, &frame).unwrap();
 }

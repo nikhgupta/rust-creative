d6ebb2078c3748569b4fd80663d828eb6cbfaa0e
Nikhil Gupta <me@nikhgupta.com>
2024-03-13 00:22:05 +0530

    use LCHA colorspace for specifying colors

--- e01-circles/src/main.rs
+++ e01-circles/src/main.rs
@@ -1,3 +1,4 @@
+use nannou::color::{Lcha, Rgba};
 use nannou::prelude::*;
 use nannou::rand::rngs::StdRng;
 use nannou::rand::{Rng, SeedableRng};
@@ -11,6 +12,10 @@ fn main() {
         .update(update)
         .loop_mode(LoopMode::wait())
         .run();
+}
+
+fn rng(seed: u64) -> StdRng {
+    StdRng::seed_from_u64(seed)
 }
 
 struct Particle {
@@ -32,6 +37,7 @@ impl Particle {
 
 struct Model {
     seed: u64,
+    hue: f32,
     num: u32,
     particles: Vec<Particle>,
 }
@@ -41,6 +47,7 @@ impl Model {
         let particles = Vec::new();
 
         Model {
+            hue: 0.0,
             seed: 0,
             num: 100,
             particles,
@@ -56,28 +63,40 @@ impl Model {
         self
     }
 
+    fn reset_hue(&mut self) -> &mut Self {
+        self.hue = rng(self.seed).gen_range(0.0..360.0);
+        self
+    }
+
     fn generate(&mut self, app: &App) -> &mut Self {
-        self.particles = Vec::new();
-        let mut rng = StdRng::seed_from_u64(self.seed);
-
-        // instead of relying on fixed width and height,
-        // we want to use window dimensions on the fly.
-        // added a margin to the window dimensions to avoid particles
-        // from sticking to the edges of the window.
         let rect = app.window_rect().pad(MARGIN);
+        let mut rng = rng(self.seed);
 
-        // we want to add a bunch of particles to the model
+        self.particles = Vec::new();
+
         for _ in 0..self.num {
+            // you can find details about the LCHA in nannou here:
+            // https://docs.rs/nannou/latest/nannou/color/struct.Lch.html#fields
+            // we have a hue, and want to generate particles with varying
+            // lightness and chroma, so we use LCHA
+            // for particles, we want neither too dark nor too light
+            let color: Rgba = Lcha::new(
+                rng.gen_range(40.0..90.0),
+                rng.gen_range(40.0..60.0),
+                self.hue,
+                0.25 * 0.75 + rng.gen::<f32>(),
+            )
+            // notice the .into() here
+            // this is a trait that converts from one type to another
+            // in this case, it converts from Lcha to Rgba
+            // which is expected by our Particle struct
+            .into();
+
             let dot = Particle {
                 x: rng.gen_range(-0.5..0.5) * rect.w(),
                 y: rng.gen_range(-0.5..0.5) * rect.h(),
-                radius: rng.gen_range(40.0..200.0),
-                color: Rgba::new(
-                    rng.gen(),
-                    rng.gen(),
-                    rng.gen(),
-                    0.25 * 0.75 + rng.gen::<f32>(),
-                ),
+                radius: rng.gen_range(10.0..80.0),
+                color,
             };
             self.particles.push(dot);
         }
@@ -85,19 +104,25 @@ impl Model {
         self
     }
 
-    // instead, of using a for loop to draw each particle in view,
-    // we can write a display function in the impl block of Model
-    // and delegate the drawing to each particle.
-    // this way, we can keep the view function clean and simple.
     fn display(&self, draw: &nannou::Draw) {
         for particle in &self.particles {
-            // delegate the drawing to each particle
             particle.display(&draw);
         }
     }
 
     fn reset(&mut self, app: &App) {
-        self.reset_seed().generate(&app);
+        self.reset_seed().reset_hue().generate(&app);
+    }
+
+    fn add_particles(&mut self, app: &App, num: i32) -> &mut Self {
+        // python: self.num = max(1, self.num + num);
+        // the typecasting here is wrong since we are casting num to i32
+        // and then back to u32, which would give wrong results for large
+        // numbers, **silently**.
+        // But, for our sample problem, this works without complications.
+        self.num = (self.num as i32 + num).max(1) as u32;
+        self.generate(&app);
+        self
     }
 }
 
@@ -114,7 +139,7 @@ fn model(app: &App) -> Model {
     // which annoys rust-analyzer
     // for now, I am using this, but will try to use the builder pattern later
     let mut model = Model::new();
-    model.reset_seed().generate(app);
+    model.reset(app);
     model
 }
 
@@ -123,18 +148,15 @@ fn key_pressed(app: &App, model: &mut Model, key: Key) {
         Key::Space => {
             model.reset(app);
         }
-        // add keyboard controls to increase or decrease the number of particles
-        // for now, this resets all particles.
-        // later, we will add or remove particles without resetting all of them.
         Key::Up => {
-            model.num += 10;
-            model.reset(app);
+            // since the rng generator is initialized with the seed
+            // in generate fn, we can just call generate again
+            // to add/remove particles and not reset existing
+            // particles.
+            model.add_particles(app, 1);
         }
         Key::Down => {
-            if model.num > 10 {
-                model.num -= 10;
-                model.reset(app);
-            }
+            model.add_particles(app, -1);
         }
         _ => (),
     }
@@ -144,7 +166,12 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(SNOW);
+
+    // instead of using SNOW by default,
+    // lets hook into the LCHA color space
+    // and use the hue in the model in a very light background
+    let bg_color: Rgba = Lcha::new(90.0, 12.8, model.hue, 1.0).into();
+    draw.background().color(bg_color);
 
     // use display method to draw all particles
     model.display(&draw);
